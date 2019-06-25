bebff0de79d29983e17cc6ddd02a3c33ece75d48
yb123speed <904896209@qq.com>
2019-06-25 11:08:05 +0800

    variables

--- 0x02_variables/README.md
+++ 0x02_variables/README.md
@@ -1,3 +1,33 @@
 ## 声明一个变量
 
-`var name type` 是声明单个变量的语法
+`var name type` 是声明单个变量的语法。
+
+## 声明具有初始值的变量
+
+`var name type = initialvalue` 是声明具有初始值的变量的语法。
+
+## 类型推断
+
+如果使用语法 `var name = initialvalue` 声明变量，Go将自动从初始值推断出该变量的类型。
+
+## 多变量声明
+
+`var name1，name2 type = initialvalue1，initialvalue2` 是多变量声明的语法。
+
+
+## 在某些情况下，我们可能希望在单个语句中声明属于不同类型的变量。这样做的语法是:
+
+```go
+var (  
+      name1 = initialvalue1
+      name2 = initialvalue2
+)
+```
+
+## 简短的声明
+
+`name := initialvalue` 是声明变量的简写语法。
+
+`:=` 左侧没有新的变量,它会抛出错误no new variables on left side of :=。即 `:=` 左侧至少有一个新变量。
+
+由于Go是强类型的，因此声明属于一种类型的变量不能赋予另一种类型的值。
--- 0x02_variables/variables.go
+++ 0x02_variables/variables.go
@@ -9,5 +9,28 @@ func main(){
 	fmt.Println("my age is",age)
 	age = 54
 	fmt.Println("my age is",age)
+
+	var age1 int = 29
+	fmt.Println("age1 is", age1)
+
+	var age2 = 29
+	fmt.Println("age2 is", age2)
+
+	//var age3, age4 int = 33, 34
+	var age3, age4 = 33, 34
+	fmt.Println("age3 is", age3," age4 is",age4)
+
+	var (
+		name	= "Yebin"
+		age5		= 29
+		height int
+	)
+	fmt.Println("name is ",name, " age is ", age5, "height is", height)
+
+	name1, age6 := "naveen", 29 //short hand declaration
+    fmt.Println("my name is", name1, "age is", age6)
+	
+	name1, age7 := "yebin", 34
+	fmt.Println("my name is", name1, "age is", age7)
 }
 
