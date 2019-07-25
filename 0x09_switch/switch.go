c231b2fc385b752599ff88450efdb2eb3cbdb769
yb123speed <904896209@qq.com>
2019-07-25 17:13:07 +0800

    Switch

---
+++ 0x09_switch/switch.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	finger:=4
+	switch finger{
+	case 1:
+		fmt.Println("Thumb")
+	case 2:
+		fmt.Println("Index")
+	case 3:
+		fmt.Println("Middle")
+	case 4:
+		fmt.Println("Ring")
+	case 5:
+		fmt.Println("Pinky")
+	}
+
+	//Default Case
+	switch finger:=8;finger{
+	case 1:
+		fmt.Println("Thumb")
+	case 2:
+		fmt.Println("Index")
+	case 3:
+		fmt.Println("Middle")
+	case 4:
+		fmt.Println("Ring")
+	case 5:
+		fmt.Println("Pinky")
+	default:
+		fmt.Println("incorrect finger number")
+	}
+
+	num := 75
+    switch { // expression is omitted
+    case num >= 0 && num <= 50:
+        fmt.Println("num is greater than 0 and less than 50")
+    case num >= 51 && num <= 100:
+        fmt.Println("num is greater than 51 and less than 100")
+    case num >= 101:
+        fmt.Println("num is greater than 100")
+	}
+	
+	switch num := number(); { //num is not a constant
+    case num < 50:
+        fmt.Printf("%d is lesser than 50\n", num)
+        fallthrough
+    case num < 100:
+        fmt.Printf("%d is lesser than 100\n", num)
+        fallthrough
+    case num < 200:
+        fmt.Printf("%d is lesser than 200", num)
+    }
+	
+}
+
+func number() int {  
+	num := 15 * 5 
+	return num
+}
